423df3092e64f6537be6d7edf10fe9328922c0be
Imel23 <imad.elbouazzaoui.ee@gmail.com>
2025-11-05 20:25:44 +0100

    Update username in configuration and validation files; add finder and writer scripts

---
+++ finder-app/writer.sh
@@ -0,0 +1,15 @@
+write_file=$1
+write_str=$2
+
+if [ -z "$write_file" ] || [ -z "$write_str" ]; then
+    exit 1
+fi
+
+mkdir -p "$(dirname "$write_file")"
+
+echo "$write_str" > "$write_file"
+
+if [ $? -ne 0 ]; then
+    echo "Error: Could not write to file '$write_file'."
+    exit 1
+fi
